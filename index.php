76d02691272b531c617808b6cdb8b0dd80bbdb5c
fedepaloz <palozzifederico@gmail.com>
2022-08-30 16:09:07 +0200

    sanck 1 finished

--- index.php
+++ index.php
@@ -33,14 +33,36 @@ $partite = [
     
 ];
 
-for ($i=0; $i < count($partite) ; $i++) { 
+/* for ($i=0; $i < count($partite) ; $i++) { 
     echo ($partite[$i]["casa"]);
     echo ($partite[$i]["ospite"]);
     echo($partite[$i]["punti-casa"]);
     echo($partite[$i]["punti-ospite"]);
 
-}
+} */
 
 ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php for ($i=0; $i < count($partite) ; $i++) : ?> 
+        <li>
+            <h1><?=$partite[$i]["casa"]?></h1>
+            <h2>20</h2>
+            <h1>lazio</h1>
+            <h2>10</h2>
+         
+        </li>
+    <?php endfor; ?> 
+    </ul>
+</body>
+</html>
