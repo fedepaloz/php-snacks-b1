3c480d3c636f9f74ef43818098f08f752acd1e81
fedepaloz <palozzifederico@gmail.com>
2022-08-30 16:50:56 +0200

    snack 2 work in progress

--- snack-1-index.php
+++ snack-1-index.php
@@ -1,7 +1,3 @@
-<!-- 
-    Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60
- -->
 
 <?php
 
@@ -66,3 +62,4 @@ $partite = [
     </ul>
 </body>
 </html>
+
---
+++ snack-2-index.php
@@ -0,0 +1,18 @@
+<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”; -->
+
+
+<?php
+
+$mail= $_GET['mail'] ?? '';
+
+if  (!str_contains($mail,'@') || !str_contains($mail,'.') ) {
+   {
+        echo 'accesso negato';
+    }
+    
+else {
+    echo 'accesso riuscito';
+}
+
+?>
+
